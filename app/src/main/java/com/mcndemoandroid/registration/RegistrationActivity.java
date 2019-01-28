287f9b3112b1f8c626ddf6586fcf5d5807f80b23
expertAbhisarika <abhisarika111111@gmail.com>
2019-01-28 17:11:38 +0530

    registration

---
+++ app/src/main/java/com/mcndemoandroid/registration/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.mcndemoandroid.registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mcndemoandroid.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
